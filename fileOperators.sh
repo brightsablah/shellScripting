bc654d465cf8cb706f17c694cbd82c0ad86cbb9a
Bright <bright.sablah@outlook.com>
2023-08-26 20:27:56 +0000

    adding file operators notes

---
+++ fileOperators.sh
@@ -0,0 +1,12 @@
+# File Operators
+
+# -d file 	True if the file is a directory
+# -e file	True if file exists
+# -f file 	True if provided strinf is a file
+# -g file	True if the group id set on a file
+# -r file	True if the file has a non-zero size
+# -u file	True if the user id is set on a file
+# -w file 	True if the file is writable
+# -x file	True if file is an executable
+#
+
